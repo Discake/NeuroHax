6c3986977323f3413e1cd5a7aa611f4bdd9f09fc
Discake <discakelog@gmail.com>
2025-10-30 00:42:01 +0300

    cuda fix in main

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from App import App
+import multiprocessing as mp
+
 
 if __name__ == '__main__':
     app = App(play=False, train=True, draw=False, logging=True)
     # app.start_ai_game()
+    mp.set_start_method('spawn', force=True)
+
     training = app.training(app.map)
     training.train(True)
