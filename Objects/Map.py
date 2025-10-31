dc8167ef19210988ace4900abd821fc13428fabd
Discake <discakelog@gmail.com>
2025-10-31 17:34:51 +0300

    Objects.Map refactoring

--- Objects/Map.py
+++ Objects/Map.py
@@ -2,25 +2,20 @@ import torch
 from Objects.Ball import Ball
 from Objects.Player import Player
 import Constants
-import numpy as np
-import copy
 import random
 
 from Physics.WallCollision import WallCollision
 
 class Map:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+    def __init__(self):
         self.balls = []
-        self.players = []
+
+        self.players_team1 = []
+        self.players_team2 = []
         self.gates = []
         self.walls = []
         self.score = [0, 0]
-        self.save = None
-        self.hit_flag = False
         self.kick_flag = False
-        self.wall_hit = False
 
     def set_field(self, field):
         self.field = field
@@ -34,105 +29,93 @@ class Map:
     
     def add_players(self):
         for i in range(Constants.player_number):
-            player1 = Player(
+            player = Player(
                 Constants.players_positions_team1[i],
                 Constants.player_radius,
                 Constants.player_mass,
                 Constants.max_player_speed)
-            player1.set_color(Constants.player_color)
-            self.players.append(player1)
-            self.balls.append(player1)
-
-    def set_ball_teams(self, ball_teams):
-        self.ball_teams = ball_teams
+            player.set_color(Constants.player_color)
+            self.players_team1.append(player)
+            self.balls.append(player)
 
     def move_balls(self):
-        
+        all_balls = self.balls + self.players_team1 + self.players_team2
+
+        # Итерации нужны для обеспечения разрешимости коллизий при высоких скоростях
         for _ in range(Constants.iterations):
-            for team in self.ball_teams:
-                for ball in team:
-                    ball.is_collided = False
-                    for other_ball in team:
-                        if ball != other_ball and not ball.is_collided :
-                            if ball.detect_collision(other_ball):
-                                ball.resolve_collision(other_ball)
-                                ball.is_collided = True
-                                if(isinstance(ball, Player) or isinstance(other_ball, Player)):
-                                    self.hit_flag = True
-                                    self.hit_player = other_ball
+            for ball in all_balls:
+                ball.move(Constants.time_increment / Constants.iterations)
 
-                    ball.move(Constants.time_increment / Constants.iterations)
+            for i in range(len(all_balls)):
+                ball = all_balls[i]
+                ball.is_collided = False
 
-                    for gate in self.gates:
-                        for line in gate.boundaries:
-                            if line.detect_collision(ball):
-                                line.resolve_collision(ball)
-                    for wall in self.walls:
-                        if wall.detect_collision(ball):
-                            wall.resolve_collision(ball)
-                            self.wall_hit = True
+                # Коллизия шаров друг с другом
+                for j in range(i + 1, len(all_balls)):
+                    other_ball = all_balls[j]
+                    if ball.detect_collision(other_ball):
+                        ball.resolve_collision(other_ball)
+
+                # Коллизия шаров и ворот
+                for gate in self.gates:
+                    for line in gate.boundaries:
+                        if line.detect_collision(ball):
+                            line.resolve_collision(ball)
+
+                # Пинок мяча
+                if isinstance(ball, Player):
+                    for other_ball in self.balls:
+                        if not ball.is_kicking:
+                            self.kick(ball, other_ball) 
+            
+            # Коллизия мяча и границы поля
+            for ball in self.balls:
+                for wall in self.walls:
+                    if wall.detect_collision(ball):
+                        wall.resolve_collision(ball)
+                        self.wall_hit = True          
 
 
-                    if isinstance(ball, Player):
-                        for other_ball in team:
-                            if not isinstance(other_ball, Player) and ball.is_kicking:
-                                ball_pos = ball.position
-                                other_ball_pos = other_ball.position
+    def kick(self, ball, other_ball):
+        ball_pos = ball.position
+        other_ball_pos = other_ball.position
                                 
-                                direction = ball_pos - other_ball_pos
-                                dist = torch.dot(direction, direction)
-                                if dist < (ball.radius + other_ball.radius + Constants.kick_radius) ** 2:
-                                    ball.kick(other_ball, direction)
-                                    self.kick_flag = True
+        direction = ball_pos - other_ball_pos
+        dist = torch.dot(direction, direction)
+        if dist < (ball.radius + other_ball.radius + Constants.kick_radius) ** 2:
+            ball.kick(other_ball, direction)
+            self.kick_flag = True
 
-
-            
-            # if not isinstance(ball, Player):
-            #     if ball.position.x <= Constants.left_gates_center_x + Constants.gates_outer_width:
-            #         self.score[1] += 1
-            #         self.load()
-            #         self.save_state()
-            #     if ball.position.x >= Constants.right_gates_center_x:
-            #         self.score[0] += 1
-            #         self.load()
-            #         self.save_state()
-            
-
-    def save_state(self):
-        # self.save = copy.deepcopy(self.ball_teams)
-
-        # for team in self.ball_teams:
-        #     for ball in team:
-        #         print("Saved: ")
-        #         print(f"{ball}")
-        pass
-
-    def load(self):
+    def load_random(self): # Метод загрузки первого игрока на поле в случайной позиции
 
         self.kick_flag = False
         self.hit_flag = False
-        # self.ball_teams.clear()
-
-        # for _ in range(num):
-        #     self.ball_teams.append(copy.deepcopy(self.balls))
         
-        # self.ball_teams = self.save
         is_right = random.randint(0, 1)
-        y_pos = random.randint(0, Constants.field_size[1] - self.ball_teams[0][0].radius)
-
-
+        y_pos = random.randint(0, Constants.field_size[1] - self.players_team1[0].radius)
 
         if is_right:
-            self.ball_teams[0][0].position[0] = Constants.players_positions_team1[0][0]
+            self.players_team1.position[0] = Constants.players_positions_team1[0][0]
         else:
-            self.ball_teams[0][0].position[0] = Constants.players_positions_team2[0][0]
-        self.ball_teams[0][0].position[1] = Constants.field_margin / 2 + y_pos
+            self.players_team1[0].position[0] = Constants.players_positions_team2[0][0]
+        self.players_team1[0].position[1] = Constants.field_margin / 2 + y_pos
 
-        self.ball_teams[0][1].position = torch.tensor([Constants.x_center, Constants.y_center], device=Constants.device)
-        # self.players.clear()
-        # for team in self.ball_teams:
-        #     for i in range(len(team) - 1):
-        #         self.players.append(team[i])
+        self.balls[0].position = torch.tensor([Constants.x_center, Constants.y_center], device=Constants.device)
+
+    def load_random(self): # Метод загрузки всех игроков и мячей на начальные позиции
+        self.kick_flag = False
+
+        for i, player in enumerate(self.players_team1):
+            player.position = Constants.players_positions_team1[i]
+            player.velocity = torch.tensor([0., 0.], device=Constants.device)
+
+        for i, player in enumerate(self.players_team2):
+            player.position = Constants.players_positions_team2[i]
+            player.velocity = torch.tensor([0., 0.], device=Constants.device)
+
+        for i, ball in enumerate(self.balls):
+            ball.position = Constants.balls_positions[i]
+            ball.velocity = torch.tensor([0., 0.], device=Constants.device)     
     
     def add_balls(self):
         for i in range(Constants.ball_number):
@@ -141,6 +124,7 @@ class Map:
             
             self.balls.append(ball)
 
+# метод клонирования объектов карты
 def clone_and_detach_object(obj):
     if hasattr(obj, '__dict__'):
         cloned_obj = obj.__class__.__new__(obj.__class__)
@@ -158,6 +142,7 @@ def clone_and_detach_object(obj):
     else:
         return obj
 
+# Метод клонирования карты
 def clone_and_detach_map(old_map):
     new_map = Map(old_map.width, old_map.height)
     new_map.score = list(old_map.score)
