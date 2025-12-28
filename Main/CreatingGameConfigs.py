5c5203a966a8154b4bac99e66e3d6a20c616e144
Discake <Discakelog@gmail.com>
2025-12-28 05:43:07 +0300

    Некоторые изменения физики и размеров ворот в конфигурации игры

--- Main/CreatingGameConfigs.py
+++ Main/CreatingGameConfigs.py
@@ -5,15 +5,15 @@ map_height = 800
 
 map = MapConfig(map_width, map_height)
 
-field_width = 600
-field_height = 400
+field_width = 800
+field_height = 600
 field_x_center = 500
 field_y_center = 400
 
 field = FieldConfig(field_width, field_height, field_x_center, field_y_center)
 
 gate_width = 50
-gate_height = 100
+gate_height = 200
 
 right_gate_x_center = field.x_center + field.width / 2 + gate_width / 2
 left_gate_x_center = field.x_center - field.width / 2 - gate_width / 2
@@ -31,9 +31,9 @@ physics_player_mass = 1
 physics_kick_radius = 15
 physics_kick_power = 3
 physics_friction = 0.3
-physics_player_max_speed = 10
+physics_player_max_speed = 7
 physics_ball_max_speed = 30
-physics_player_move_modificator = 0.5
+physics_player_move_modificator = 0.1
 
 physics = PhysicsConfig(physics_tick_rate, physics_ball_radius, physics_player_radius,
                          physics_ball_mass, physics_player_mass, physics_kick_radius,
@@ -58,12 +58,12 @@ bottom_wall = WallConfig((field.x_center - field.width / 2, field.y_center - fie
 top_wall = WallConfig((field.x_center - field.width / 2, field.y_center + field.height / 2),
                       (field.x_center + field.width / 2, field.y_center + field.height / 2))
 left_wall1 = WallConfig((field.x_center - field.width / 2, field.y_center - field.height / 2),
-                        (field.x_center - field.width / 2, gate_y_center - gate_height))
-left_wall2 = WallConfig((field.x_center - field.width / 2, gate_y_center + gate_height),
+                        (field.x_center - field.width / 2, gate_y_center - gate_height / 2))
+left_wall2 = WallConfig((field.x_center - field.width / 2, gate_y_center + gate_height / 2),
                         (field.x_center - field.width / 2, field.y_center + field.height / 2))
 right_wall1 = WallConfig((field.x_center + field.width / 2, field.y_center - field.height / 2),
-                        (field.x_center + field.width / 2, gate_y_center - gate_height))
-right_wall2 = WallConfig((field.x_center + field.width / 2, gate_y_center + gate_height),
+                        (field.x_center + field.width / 2, gate_y_center - gate_height /2 ))
+right_wall2 = WallConfig((field.x_center + field.width / 2, gate_y_center + gate_height / 2),
                         (field.x_center + field.width / 2, field.y_center + field.height / 2))
 
 general_walls = [left_gate_wall1, left_gate_wall2, left_gate_wall3, right_gate_wall1, right_gate_wall2, right_gate_wall3]
